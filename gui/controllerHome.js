88c9b5ab0cad5e9aed2039ddc4558f32e7ecfa35
Gilles Volluz <gilles.volluz@gmail.com>
2016-10-30 12:31:18 +0100

    Création UI sauvegarde/chargement personnages

--- gui/controllerHome.js
+++ gui/controllerHome.js
@@ -1,11 +1,5 @@
 //CONTROLLER - HOME
-paragonApp.controller('homeController', ['$scope', '$cookies', '$log', 'personnageService', function($scope, $cookies, $log, personnageService) {  
-
-    var clickEvent = new MouseEvent("click", {
-            "view": window,
-            "bubbles": true,
-            "cancelable": false
-        });
+paragonApp.controller('homeController', ['$scope', '$cookies', '$log', 'personnageService', function($scope, $cookies, $log, personnageService) { 
     
     $scope.constantes = personnageService.constantes;
     $scope.personnage = personnageService.personnage;
@@ -13,48 +7,13 @@ paragonApp.controller('homeController', ['$scope', '$cookies', '$log', 'personna
     $scope.etapeActuelle = 'Bienvenue';
     $scope.$parent.pagePrecedente = '#/';
     $scope.$parent.pageSuivante = '#/paragonStep1/';
-    
-    $scope.formatsExportation = ['json', 'LaTeX', 'pdf', 'csv'];
-    $scope.formatExportation = 'json';
 
     $scope.personnage.creer = function(){
-        personnageService.personnageRAZ(true);
+        personnageService.personnage.remiseAZero(true);
     }
     
-    $scope.personnage.exporter = function(){
-        var perso = personnageService.exporter($scope.formatExportation);
+    $scope.exporter = function(){
         
-        var type = 'text/plain';
-        var ext = '.txt';
-        
-        switch($scope.formatExportation){
-            case 'json':
-                type = 'text/json';
-                ext = '.json';
-                break;
-            case 'LaTeX':
-                type = 'application/x-tex';
-                ext = '.tex';
-                break;
-            case 'csv':
-                type = 'text/csv';
-                ext = '.csv';
-                break;
-            case 'pdf':
-                type = 'application/pdf';
-                ext = '.pdf';
-                break;
-        }
-        
-        var formBlob = new Blob([perso], { type: type });
-        var a = document.getElementById('personnageExport');
-        a.setAttribute('href', window.URL.createObjectURL(formBlob));
-        a.download = $scope.personnage.metatype+'_'+$scope.personnage.nom+ext; 
-        a.dispatchEvent(clickEvent);
-    }
-
-    $scope.personnage.aleatoire = function(){
-        personnageService.personnageAleatoire();
     };
 
 }]);
